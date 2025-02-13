c7ed9a497bc493c172e570439fa00416cab77ce0
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2025-02-14 00:58:40 +0900

    feat: initial tests

--- landing_page/tests.py
+++ landing_page/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class LandingPageTest(TestCase):
+    
+    def test_home(self) -> None:
+        c = Client()
+        home_url = reverse("landing_page:home")
+        
+        self.assertEqual(c.get(home_url).status_code, 200)
+        self.assertTemplateUsed(c.get(home_url), "landing_page.html")
