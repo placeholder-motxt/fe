134a0a77fb801774d04e46992e82bd3a65a108ac
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2025-03-21 00:02:53 +0700

    [RED] change TDD of js from babel-jest to QUnits

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,25 +1,30 @@
-// Add DragEvent polyfill
-class MockDragEvent extends Event {
-    constructor(type, options) {
-      super(type, options);
-      this.dataTransfer = options?.dataTransfer || { files: [] };
-    }
-  }
-  global.DragEvent = MockDragEvent;
-  
-  // Keep existing mocks
-  jest.mock('*.css', () => ({}));
-  jest.mock('*.svg', () => ({}));
-  
-  class MockFormData {
-    append = jest.fn();
-  }
-  global.FormData = MockFormData;
-  
-  global.URL.createObjectURL = jest.fn(() => 'mock-url');
-  global.URL.revokeObjectURL = jest.fn();
-  
-  Object.defineProperty(document, 'cookie', {
-    writable: true,
-    value: 'csrftoken=testtoken123'
-  });
+// Use CommonJS require syntax for polyfill
+const { TextEncoder, TextDecoder } = require('@sinonjs/text-encoding');
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
+
+// Now import other dependencies
+const { JSDOM } = require('jsdom');
+const fs = require('fs');
+const path = require('path');
+
+// Load HTML template
+const html = fs.readFileSync(
+  path.resolve(__dirname, '../templates/convert_page.html'),
+  'utf8'
+);
+const dom = new JSDOM(html, { runScripts: 'dangerously' });
+
+// Setup global objects
+global.window = dom.window;
+global.document = dom.window.document;
+global.fetch = require('jest-fetch-mock');
+global.navigator = dom.window.navigator;
+global.localStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+};
+
+// Load the actual JS file
+require('../static/js/convert_page.js');
--- tests/styleMock.js
+++
@@ -1,2 +0,0 @@
-// tests/styleMock.js
-module.exports = {};
