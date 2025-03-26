4a0f2fc4c634ab2bd3aae9440ff1590b060007b8
KenKomKom <kenichikomala@gmail.com>
2025-03-26 21:45:36 +0700

    [CHORE] added configurations for prometheus

--- fe/urls.py
+++ fe/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django_prometheus import exports
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,6 @@ urlpatterns = [
     path('', include("convert_page.urls")),
     path('repository/', include("repository.urls")),
     path('doc/', include('user_manual.urls')),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    path("metrics/", exports.ExportToDjangoView, name="metrics"),
 ]
