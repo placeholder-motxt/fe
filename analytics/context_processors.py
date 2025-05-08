07abface830379bff54d946fd2b1902ecddc7dfa
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2025-05-08 21:15:44 +0700

    [GREEN] change implementaion of context_processor

--- analytics/context_processors.py
+++ analytics/context_processors.py
@@ -1,8 +1,7 @@
-def google_analytics(request):
+def google_analytics():
     """
     Add Google Analytics tracking ID to the context
     """
-    request = request
     return {
         'GA_TRACKING_ID': 'G-PD1C44V6LL'  # Updated tracking ID
     }
