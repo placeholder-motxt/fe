f91c5bc3267430f0d7e82f0fee31e98420ff87fc
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2025-05-08 21:09:37 +0700

    [REFACTOR] remove unused request for code quality issue

--- analytics/context_processors.py
+++ analytics/context_processors.py
@@ -2,6 +2,7 @@ def google_analytics(request):
     """
     Add Google Analytics tracking ID to the context
     """
+    request = request
     return {
         'GA_TRACKING_ID': 'G-PD1C44V6LL'  # Updated tracking ID
     }
